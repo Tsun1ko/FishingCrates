577a00552ec2d0e4000a16ccac3e6e42a717e80f
Tsun1ko <cuzimmrdna@gmail.com>
2024-11-20 22:26:14 +0100

    added wooden crate block and fishing crates item group

--- src/main/java/net/tsuniko/fishingcrates/FishingCrates.java
+++ src/main/java/net/tsuniko/fishingcrates/FishingCrates.java
@@ -2,23 +2,20 @@ package net.tsuniko.fishingcrates;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tsuniko.fishingcrates.block.ModBlocks;
+import net.tsuniko.fishingcrates.item.ModItemGroups;
+import net.tsuniko.fishingcrates.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FishingCrates implements ModInitializer {
 	public static final String MOD_ID = "fishingcrates";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
