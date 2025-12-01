790d8208795b8837760fd360887f9d8df17ed5bd
Tsun1ko <cuzimmrdna@gmail.com>
2025-12-01 10:53:52 +0100

    initial commit

--- src/main/java/net/tsuniko/fishingcrates/FishingCratesClient.java
+++ src/main/java/net/tsuniko/fishingcrates/FishingCratesClient.java
@@ -1,10 +1,31 @@
 package net.tsuniko.fishingcrates;
 
-import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-public class FishingCratesClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = FishingCrates.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = FishingCrates.MODID, value = Dist.CLIENT)
+public class FishingCratesClient {
+    public FishingCratesClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
 
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        FishingCrates.LOGGER.info("HELLO FROM CLIENT SETUP");
+        FishingCrates.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
