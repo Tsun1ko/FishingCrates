2774d51ced942209929042858c10fc0c17e40177
Tsun1ko <cuzimmrdna@gmail.com>
2026-03-23 20:07:45 +0100

    migrated to mojang mappings

--- src/main/java/net/tsuniko/block/CrateBlock.java
+++ src/main/java/net/tsuniko/block/CrateBlock.java
@@ -1,22 +1,23 @@
 package net.tsuniko.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.ChatFormatting;
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 
 import java.util.List;
 
 public class CrateBlock extends Block {
-    public CrateBlock(Settings settings) {
+    public CrateBlock(Properties settings) {
         super(settings);
     }
 
     @Override
-    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
-        tooltip.add(Text.translatable("tooltip.fishing_crates.crate_info").formatted(Formatting.GRAY));
-        super.appendTooltip(stack, context, tooltip, options);
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltip, TooltipFlag options) {
+        tooltip.add(Component.translatable("tooltip.fishing_crates.crate_info").withStyle(ChatFormatting.GRAY));
+        super.appendHoverText(stack, context, tooltip, options);
     }
 }
